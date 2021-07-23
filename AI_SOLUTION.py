6a300ce8b2199e02a3c8c976611cf313d84e1455
arnabdas95 <dasarnab1411995@gmail.com>
2021-07-23 22:41:06 +0530

    AI solution is implemented. Get the result but path is also include the backtrack moves(issue)

--- AI_SOLUTION.py
+++ AI_SOLUTION.py
@@ -89,32 +89,45 @@ class Node():
                                                                                down_child_state[self.blank_index]
             return down_child_state
 
-
+def get_best_huristic_from_queue():
+       node_queue.sort(key=lambda x: x[1])
+       return node_queue[0]
 
 # Global variable declare
 GOAL_STATE = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-#INITIAL_STATE = [2,7,3,5,9,4,1,6,8]
+#INITIAL_STATE = [2,7,3,5,9,4,1,6,8]  #give result
 #INITIAL_STATE= [1,3,4,8,6,2,7,9,5] #easy
-INITIAL_STATE = [1, 2, 5, 4, 3, 6, 7, 9, 8]
+#INITIAL_STATE = [1, 2, 5, 4, 3, 6, 7, 9, 8]
+INITIAL_STATE = [4,1,3,7,2,5,8,9,6] #easy give result
+INITIAL_STATE = [4,2,3,7,1,6,8,9,5] #easy give result
+# GOAL_STATE=[1,2,3,8,9,4,7,6,5]
+
 
 initial_to_goal_path = []
 visited_state_set = set()
 vs =[]
 node = Node(INITIAL_STATE,None,None,0,0)
 node_queue =[]
-
+node_queue.append([INITIAL_STATE,0,None])
 #before start check if initial state is also goal state then don't run while
 if GOAL_STATE== INITIAL_STATE:
     print("win")
     exit(0)
 
-while(True):
+while(len(node_queue) >0):
+    node_queue.pop(0)
     left_move_child =node.left_move()
     right_move_child =node.right_move()
     up_move_child = node.up_move()
     down_move_child = node.down_move()
-    print(f"new state created-parent ={node.state}")
-    print(f"left_move_child={left_move_child},right_move_child= {right_move_child},up_move_child= {up_move_child},down_move_child= {down_move_child}")
+    cc=0
+    for i in range(3):
+        for j in range(3):
+         print(node.state[cc+j],end='')
+        print( "\n")
+        cc +=3
+   # print(f"new state created-parent ={node.state}")
+   # print(f"left_move_child={left_move_child},right_move_child= {right_move_child},up_move_child= {up_move_child},down_move_child= {down_move_child}")
 
     if left_move_child !=None:
        # if node.check_visited_or_not(left_move_child):
@@ -123,7 +136,8 @@ while(True):
             if left_move_child != GOAL_STATE:
               left_child_total_huristic_cost =node.heuristic_fn2(left_move_child,GOAL_STATE)+node.manhattan_distance(left_move_child, GOAL_STATE)
               node.list_of_child_huristic_cost.append(['LEFT_MOVE',left_child_total_huristic_cost])
-              print(f"left-child heuristic value{left_child_total_huristic_cost}")
+              node_queue.append([left_move_child,left_child_total_huristic_cost,'LEFT_MOVE'])
+            #  print(f"left-child heuristic value{left_child_total_huristic_cost}")
             else:
                 print("win")
                 initial_to_goal_path.append("LEFT_MOVE")
@@ -137,7 +151,8 @@ while(True):
                 right_child_total_huristic_cost = node.heuristic_fn2(right_move_child, GOAL_STATE) + node.manhattan_distance(
                     right_move_child, GOAL_STATE)
                 node.list_of_child_huristic_cost.append(['RIGHT_MOVE', right_child_total_huristic_cost])
-                print(f"rightt-child heuristic value{right_child_total_huristic_cost}")
+                node_queue.append([right_move_child, right_child_total_huristic_cost, 'RIGHT_MOVE'])
+             #   print(f"rightt-child heuristic value{right_child_total_huristic_cost}")
             else:
                 print("win")
                 initial_to_goal_path.append("RIGHT_MOVE")
@@ -151,7 +166,8 @@ while(True):
                 up_child_total_huristic_cost = node.heuristic_fn2(up_move_child, GOAL_STATE) + node.manhattan_distance(
                     up_move_child, GOAL_STATE)
                 node.list_of_child_huristic_cost.append(['UP_MOVE', up_child_total_huristic_cost])
-                print(f"up-child heuristic value{up_child_total_huristic_cost}")
+                node_queue.append([up_move_child, up_child_total_huristic_cost, 'UP_MOVE'])
+             #   print(f"up-child heuristic value{up_child_total_huristic_cost}")
             else:
                 print("win")
                 initial_to_goal_path.append("UP_MOVE")
@@ -165,30 +181,21 @@ while(True):
                 down_child_total_huristic_cost = node.heuristic_fn2(down_move_child, GOAL_STATE) + node.manhattan_distance(
                     down_move_child, GOAL_STATE)
                 node.list_of_child_huristic_cost.append(['DOWN_MOVE', down_child_total_huristic_cost])
-                print(f"down-child heuristic value{down_child_total_huristic_cost}")
+                node_queue.append([down_move_child, down_child_total_huristic_cost, 'DOWN_MOVE'])
+              #  print(f"down-child heuristic value{down_child_total_huristic_cost}")
             else:
                 print("win")
                 initial_to_goal_path.append("DOWN_MOVE")
                 break
-    print("huristic function calculated")
+   # print("huristic function calculated")
     #add best path and huristic value
-    desired_move =( node.get_best_huristic_among_all_child())
-    initial_to_goal_path.append(desired_move[0])
+    desired_move =( get_best_huristic_from_queue())
+    initial_to_goal_path.append(desired_move[2])
     print(desired_move)
-    #print(f"set{visited_state_set}")
-    #set new parent node among the child node
-    if desired_move[0]=='LEFT_MOVE':
-        node = Node(left_move_child, node.state, 'LEFT',desired_move[1],node.depth+1)
+    node = Node(desired_move[0], node.state,desired_move[2], desired_move[1], node.depth + 1)
 
-    if desired_move[0] == 'RIGHT_MOVE':
-        node = Node(right_move_child, node.state, 'RIGHT', desired_move[1], node.depth + 1)
-
-    if desired_move[0] == 'UP_MOVE':
-        node = Node(up_move_child, node.state, 'UP', desired_move[1], node.depth + 1)
-
-    if desired_move[0] == 'DOWN_MOVE':
-        node = Node(down_move_child, node.state, 'DOWN', desired_move[1], node.depth + 1)
 
     print(f"depth={node.depth},state={node.state},move={node.move}")
 
 print(initial_to_goal_path)
+print(len(initial_to_goal_path))
